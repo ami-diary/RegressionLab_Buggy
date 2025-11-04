d41d6d74aa1cdb1da5ead944622c180a869deea7
Ami <168814388+ami-diary@users.noreply.github.com>
2025-11-05 00:30:33 +0300

    Update DateUtilsTests.cs

--- tests/RegressionLab.Tests/DateUtilsTests.cs
+++ tests/RegressionLab.Tests/DateUtilsTests.cs
@@ -3,18 +3,7 @@ using RegressionLab;
 using System;
 
 namespace RegressionLab.Tests;
-
-[TestClass]
-public class DateUtilsTests
-{
-    [TestMethod]
-    public void OverlapMinutes_NormalCase()
-    {
-        var a1 = new DateTime(2025, 10, 1, 9, 0, 0);
-        var a2 = new DateTime(2025, 10, 1, 12, 0, 0);
-        var b1 = new DateTime(2025, 10, 1, 11, 0, 0);
-        var b2 = new DateTime(2025, 10, 1, 13, 0, 0);
-        Assert.AreEqual(60, DateUtils.OverlapMinutes(a1, a2, b1, b2));
+t.AreEqual(60, DateUtils.OverlapMinutes(a1, a2, b1, b2));
     }
 
     [TestMethod]
